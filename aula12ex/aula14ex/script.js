a2dfcda6f779e10c052850b82afdef8df816ec85
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2021-01-21 22:30:22 -0300

    aula 14 finalizada

--- aula12ex/aula14ex/script.js
+++ aula12ex/aula14ex/script.js
@@ -1,1 +1,23 @@
-alert('olá')
+function carregar () {
+    let msg = document.getElementById('msg')
+    let img = document.getElementById('imagem')
+
+    let data = new Date()
+    let hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if (hora >= 0 && hora < 12) {
+        img.src = './img/manha.jpg'
+        document.body.style.backgroundColor = '#788058'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = './img/tarde.jpg'
+        document.body.style.backgroundColor = '#B07F73'
+    } else {
+        img.src = './img/noite.jpg'
+        document.body.style.backgroundColor = '#183138'
+    }
+}
+
+
+
